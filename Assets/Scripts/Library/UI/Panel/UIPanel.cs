67801befbe55605607d87b704c6ada545fe55d87
toomarihon <toomarihon@naver.com>
2018-03-04 15:25:09 +0900

    주석 추가

--- Assets/Scripts/Library/UI/Panel/UIPanel.cs
+++ Assets/Scripts/Library/UI/Panel/UIPanel.cs
@@ -15,6 +15,9 @@ public abstract class UIPanel : UIObject
 	public PopupType _curPopupType = PopupType.Popup;
     [HideInInspector] public System.Action<UIPanel> OnCompleteAction;
 
+    /// <summary>
+    /// 팝업이 열리기 시작할 때 호출되는 함수.
+    /// </summary>
 	public abstract void StartOpenPopup ();
 	public abstract void StartClosePopup ();
     public virtual void ClosePopup()
